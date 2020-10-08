38bc493bc48b00d4fb90479fd840150242f67c5b
gopalakrishna ramachandra <3163730+gopalakrishna-r@users.noreply.github.com>
2020-10-08 16:48:35 +0530

    added more exercises

--- chapter_2/exercises/Exercise_4.py
+++ chapter_2/exercises/Exercise_4.py
@@ -54,40 +54,13 @@ housing_labels = strat_train_set["median_house_value"].copy()
 # impute the empty values with median
 # handle categorical values and apply transformation
 housing_num = housing.drop("ocean_proximity", axis=1)
-full_pipeline = build_transformer(housing_num)
-housing_prepared = full_pipeline.fit_transform(housing)
-
-param_grid = [
-    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
-    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
-]
-
-forest_reg = RandomForestRegressor()
-
-grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
-                           scoring='neg_mean_squared_error',
-                           return_train_score=True)
-grid_search.fit(housing_prepared, housing_labels)
-
-feature_importances = grid_search.best_estimator_.feature_importances_
-print(f"best features {feature_importances}")
-k = 5
-indices_of_top_features = indices_of_features(feature_importances,k)
-
-num_attribs = list(housing_num)
-extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
-cat_encoder = full_pipeline.named_transformers_["cat"]
-cat_one_hot_attribs = list(cat_encoder.categories_[0])
-attributes = num_attribs + extra_attribs + cat_one_hot_attribs
-
-print(sorted(zip(feature_importances, attributes), reverse=True)[:k])
 
 # evaluate system on test set
-full_pipeline = build_transformer_with_features_model(housing_numericals=housing_num,
-                                                      top_features=feature_importances,
-                                                      feature_count=k)
+full_pipeline_with_no_model = build_transformer_with_features_model(housing_numericals=housing_num,
+                                                      top_features=None,
+                                                      feature_count=5)
 
-housing_prepared = full_pipeline.fit_transform(housing)
+housing_prepared = full_pipeline_with_no_model.fit_transform(housing)
 
 n_grid = {
         'kernel': ['linear', 'rbf'],
@@ -95,23 +68,29 @@ n_grid = {
         'gamma': expon(scale=1.0),
     }
 
-svm = SVR()
-
-random_search = RandomizedSearchCV(svm, cv=5, n_iter=50, verbose=2,
+random_search = RandomizedSearchCV(SVR(), cv=5, n_iter=50, verbose=2,
                                    scoring='neg_mean_squared_error', param_distributions=n_grid,
                                    return_train_score=True, n_jobs=10)
 random_search.fit(housing_prepared, housing_labels)
-
-print("best parameters out of grid search :", random_search.best_params_)
+feature_importances = random_search.best_estimator_.feature_importances_
+print(f"best features {feature_importances}")
+k = 5
+indices_of_top_features = indices_of_features(feature_importances,k)
 
 # evaluate system on test set
-
-final_model = random_search.best_estimator_
-
+full_pipeline_with_model = build_transformer_with_features_model(housing_numericals=housing_num,
+                                                                 top_features=feature_importances,
+                                                                 feature_count=k,best_parameters=random_search.best_params_)
+full_pipeline_with_model.fit(housing, housing_labels)
 X_test = strat_test_set.drop("median_house_value", axis=1)
 y_test = strat_test_set["median_house_value"].copy()
 
 X_test = strat_test_set.drop("median_house_value", axis=1)
 y_test = strat_test_set["median_house_value"].copy()
 
-print(f"performance stat {predict_with_best_model(X_test, y_test, full_pipeline, final_model)}")
+X_test_prepared = full_pipeline_with_model.transform(X_test)
+
+final_predictions = full_pipeline_with_model.predict(X_test_prepared)
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+print(f"final prediction score {final_rmse}")
--- util/MLUtil.py
+++ util/MLUtil.py
@@ -9,6 +9,7 @@ import urllib.request
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.svm import SVR
 
 from util.TopFeatureSelector import TopFeatureSelector
 
@@ -97,13 +98,26 @@ def build_transformer(housing_numericals, cat_attribs=None):
     ])
     return full_pipeline
 
-def build_transformer_with_features(housing_numericals, top_features, feature_count, cat_attribs=None):
+
+def build_transformer_with_features_model(housing_numericals, feature_count, top_features=None, cat_attribs=None,
+                                          best_parameters=None):
     pipeline = build_transformer(housing_numericals, cat_attribs)
-    preparation_and_feature_selection_pipeline = Pipeline([
-        ('preparation', pipeline),
-        ('feature_selection', TopFeatureSelector(top_features, feature_count))
-    ])
-    return preparation_and_feature_selection_pipeline
+    pipeline_steps_ = [
+        ('preparation', pipeline)
+    ]
+    if top_features is None:
+        if best_parameters is None:
+            pass
+        else:
+            pipeline_steps_ = pipeline_steps_.append([('svm_reg', SVR(best_parameters))])
+    else:
+        if best_parameters is None:
+            pipeline_steps_ = pipeline_steps_.append([('feature_selection', TopFeatureSelector(top_features, feature_count))])
+        else:
+            pipeline_steps_ = pipeline_steps_.append([('svm_reg', SVR(best_parameters)),
+                               ('feature_selection', TopFeatureSelector(top_features, feature_count))])
+
+    return Pipeline(pipeline_steps_)
 
 
 def predict_with_best_model(X_test, y_test, full_pipeline, final_model):
@@ -120,6 +134,3 @@ def predict_with_best_model(X_test, y_test, full_pipeline, final_model):
                                                 loc=squared_errors.mean(),
                                                 scale=stats.sem(squared_errors)))
     return performance_stat
-
-
-
